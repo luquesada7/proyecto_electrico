118e8c67365396f517470ea1996140ed1c541e2b
luquesada7 <lu.quesada7@gmail.com>
2018-06-20 21:33:44 -0600

    Update pidController.c

--- pidController.c
+++ pidController.c
@@ -5,7 +5,7 @@ void calculate_u(struct pid_param *params, struct pid_calc *calc)
 {
 	// Calculo de valores actuales
 	calc->proportional = params->Kp * (params->beta * calc->setpoint - calc->procout);
-	calc->der_term = (params->alpha * params->Ts) / (params->alpha * params->Td + params->Ts);
+	calc->der_term = (params->alpha * params->Td) / (params->alpha * params->Td + params->Ts);
 	calc->derivative = calc->der_term * calc->past_derivative - calc->der_term * (calc->procout - calc->past_procout);
 	calc->ctrlsignal_u = calc->proportional + calc->integral + calc->derivative; // salida del controlador
 	// Calculo de valores anteriores
