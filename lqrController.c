d9f56396704188c8474e60aa3f5731303ec34ecb
luquesada7 <lu.quesada7@gmail.com>
2018-06-20 21:32:47 -0600

    Update lqrController.c

--- lqrController.c
+++ lqrController.c
@@ -22,6 +22,9 @@ double lqrController(
 	I_act = *I_past + r_act - y_act;
 	ctrlsig_u = K1->K1_11 * I_act - (K2->K2_11 * x->x1 + K2->K2_12 * x->x2 + K2->K2_13 * x->x3);
 
+	// Guardar valor actual 
+	*I_past = I_act;
+	
 	// Valores siguientes
 	x1_next = G->G11 * x->x1 + G->G12 * x->x2 + G->G13 * x->x3 + H->H11 * ctrlsig_u + Ke->Ke_11 * (y_act - C->C11 * x->x1 - C->C12 * x->x2 - C->C13 * x->x3);
 	x2_next = G->G21 * x->x1 + G->G22 * x->x2 + G->G23 * x->x3 + H->H21 * ctrlsig_u + Ke->Ke_21 * (y_act - C->C11 * x->x1 - C->C12 * x->x2 - C->C13 * x->x3);
