51e3356a32185d424e82d2290119dcf2ca0daf2d
luquesada7 <lu.quesada7@gmail.com>
2018-06-23 22:58:00 -0600

    Update data_reader.py
    Appending data in real time to file

--- data_reader.py
+++ data_reader.py
@@ -6,12 +6,13 @@ import serial
 
 serial_port = 'COM4';
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output.txt";
+write_to_file_path = "example.txt";
 
-output_file = open(write_to_file_path, "w+");
 ser = serial.Serial(serial_port, baud_rate)
 while True:
+    output_file = open(write_to_file_path, "a");
     line = ser.readline();
     line = line.decode("utf-8") #ser.readline returns a binary, convert to string
     print(line);
     output_file.write(line);
+    output_file.close();
