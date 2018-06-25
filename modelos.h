4f978d7cd8116bd566e09cf96125c762aa51bdfb
luquesada7 <lu.quesada7@gmail.com>
2018-06-25 15:29:22 -0600

    Update modelos.h

--- modelos.h
+++ modelos.h
@@ -1,34 +1,34 @@
-#ifndef __EULER_SOLVER_H
-#define __EULER_SOLVER_H
+#ifndef __MODELS_H
+#define __MODELS_H
 
 // Estados del modelo minimo de Bergman
 struct states {
-	double G;
-	double I;
-	double X;
-	double t;
+  double G;
+  double I;
+  double X;
+  double t;
 };
 
 // Parametros del modelo minimo de Bergman
 struct parameters {
-	double p1;
-	double p2;
-	double p3;
-	double tau;
-	double n;
-	double Gb;
-	double u;
-	double h;
+  double p1;
+  double p2;
+  double p3;
+  double tau;
+  double n;
+  double Gb;
+  double u;
+  double h;
 };
 
 // Parametros de la funcion de comida
 struct food_parameters {
-	double Dg;
-	double Ag;
-	double tmax_I;
-	double tmax_G;
-	double tiempo_comida;
-	double Vg;
+  double Dg;
+  double Ag;
+  double tmax_I;
+  double tmax_G;
+  double time_food;
+  double Vg;
 
 };
 
